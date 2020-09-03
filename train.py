d1b7817245c2fa5ab94ccfbc62e9df595160cd0d
niki <nikeinikei98@gmail.com>
2020-09-03 14:27:03 +0200

    add plot for loss

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from config import IMAGES_FILE_NAME, INPUTS_FILE_NAME
 
 
@@ -13,15 +14,14 @@ def main():
     inputs = tf.convert_to_tensor(np.load(INPUTS_FILE_NAME), dtype=float)
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(128, return_sequences=True),
-        tf.keras.layers.LSTM(128, return_sequences=True),
         tf.keras.layers.LSTM(32, return_sequences=True),
+        tf.keras.layers.LSTM(32, return_sequences=True),
+        tf.keras.layers.LSTM(16, return_sequences=True),
         tf.keras.layers.Dense(2)
     ])
     model.compile(
         loss=tf.keras.losses.mean_squared_error,
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.000001),
-        metrics=["accuracy"]
+        optimizer=tf.keras.optimizers.Adam()
     )
 
     latest = tf.train.latest_checkpoint(CHECKPOINTS_DIR)
@@ -40,13 +40,21 @@ def main():
         verbose=1)
 
     try:
-        model.fit(
+        history: tf.keras.callbacks.History = model.fit(
             x=images, 
             y=inputs,
             validation_split=0.2,
-            epochs=10,
+            epochs=300,
             callbacks=[cp_callback]
         )
+        
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
     except KeyboardInterrupt:
         print("manually interrupted")
         return
