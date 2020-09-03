256dbb0e82b05abf912df235acbb678e357943a6
niki <nikeinikei98@gmail.com>
2020-09-03 12:00:18 +0200

    create checkpoints for model

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
+import os
 import numpy as np
 import tensorflow as tf
 from config import IMAGES_FILE_NAME, INPUTS_FILE_NAME
+
+
+CHECKPOINTS_PATH = "saved/cp.ckpt"
+CHECKPOINTS_DIR = os.path.dirname(CHECKPOINTS_PATH)
 
 
 def main():
@@ -8,18 +13,43 @@ def main():
     inputs = tf.convert_to_tensor(np.load(INPUTS_FILE_NAME), dtype=float)
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(16, return_sequences=True),
-        tf.keras.layers.LSTM(16, return_sequences=True),
-        tf.keras.layers.LSTM(4, return_sequences=True),
+        tf.keras.layers.LSTM(128, return_sequences=True),
+        tf.keras.layers.LSTM(128, return_sequences=True),
+        tf.keras.layers.LSTM(32, return_sequences=True),
         tf.keras.layers.Dense(2)
     ])
     model.compile(
         loss=tf.keras.losses.mean_squared_error,
-        optimizer=tf.keras.optimizers.Adam(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.000001),
         metrics=["accuracy"]
     )
 
-    model.fit(x=images, y=inputs, epochs=10)
+    latest = tf.train.latest_checkpoint(CHECKPOINTS_DIR)
+    if latest:
+        print("continuing from checkpoint")
+        model.load_weights(latest)
+
+    print("using tensorflow version: ", tf.version.VERSION)
+
+    print("images.shape", images.shape)
+    print("inputs.shape", inputs.shape)
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=CHECKPOINTS_PATH,
+        save_weights_only=True,
+        verbose=1)
+
+    try:
+        model.fit(
+            x=images, 
+            y=inputs,
+            validation_split=0.2,
+            epochs=10,
+            callbacks=[cp_callback]
+        )
+    except KeyboardInterrupt:
+        print("manually interrupted")
+        return
 
 
 if __name__ == '__main__':
